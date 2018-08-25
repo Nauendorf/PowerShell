51e996c6ad50d043fd5d2ff45f7a1d705d0da4fa
David Nauendorf <david.nauendorf@iinet.net.au>
2018-08-25 21:34:34 +0800

    Check all domains for existing samAccountName

---
+++ Check-AllDomains.ps1
@@ -0,0 +1,18 @@
+﻿function Check-AllDomains ([string]$Identity )
+{
+    'LDAP://DC=',
+    'LDAP://DC=',
+    'LDAP://DC='|
+    %{
+
+        $LDAPFilter = "(samAccountName=$Identity)"
+        $searcher = New-Object DirectoryServices.DirectorySearcher
+        $searcher.Filter = $LDAPFilter
+        $Searcher.SearchRoot = $SearchRoot
+        [adsi]$userObj = ($searcher.FindAll()).Path
+        [array]$result += $userObj
+    }
+
+    if     (!$result) {return $false}
+    elseif ($result)  {return $true}
+}
